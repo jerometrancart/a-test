75fb1cf0c5d60d90965b8ff4bfe21a5667928c81
jerometrancart <jerome.trancart@gmail.com>
2022-08-17 02:06:46 +0200

    pagination on her way

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 
 // == Import
-import './styles.css';
+
 import * as React from 'react';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
