f0645fd9125abd95a6bd4cac3dd46c19a2836ac6
jerometrancart <jerome.trancart@gmail.com>
2022-08-17 10:29:40 +0200

    fixed imports in the App.js, added hooks from react and axios to fetch the data

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,9 @@
 // == Import npm
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import Records from './components/Records';
+import Pagination from './components/Pagination';
 
 // == Import
 
@@ -24,7 +28,13 @@ const nPages = data.length
 // User is currently on this page
 const [currentPage, setCurrentPage] = useState(1);
 
+// ONLY IF THERE ARE TO MANY DATA TO DISPLAY IN ONE PAGE, WHICH IS NOT THE CASE BECAUSE I ONLY CREATED 10 ITEMS IN MOCKEROO
+// const [recordsPerPage] = useState(10);
 
+// const indexOfLastRecord = currentPage * recordsPerPage;
+// const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+// const currentRecords = data.slice(indexOfFirstRecord, indexOfLastRecord);
+// const nPages = Math.ceil(data.length / recordsPerPage)
 
 
 // == Composant
