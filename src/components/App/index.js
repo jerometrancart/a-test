b4530dcf6f84d04f844bc90ab7942631451e1bc2
jerometrancart <jerome.trancart@gmail.com>
2022-08-17 10:04:40 +0200

    Add an example of data from mockaroo, try to build the logic for the app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,11 +2,33 @@
 import React from 'react';
 
 // == Import
-import './styles.css';
+
+// To hold a table data
+const [data, setData] = useState([])
+const [loading, setLoading] = useState(true);
+
+const nPages = data.length
+
+// User is currently on this page
+const [currentPage, setCurrentPage] = useState(1);
+
+const nextPage = () => {
+  if(currentPage !== nPages) 
+      setCurrentPage(currentPage + 1)
+}
+const prevPage = () => {
+  if(currentPage !== 1) 
+      setCurrentPage(currentPage - 1)
+}
+
 
 // == Composant
 const App = () => (
-  <Nav />
+  <Pagination
+    nPages = { nPages }
+    currentPage = { currentPage } 
+    setCurrentPage = { setCurrentPage }
+/>
 );
 
 // == Export
